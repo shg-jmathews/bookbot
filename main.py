4f113b7a4c816d41f0e176b946540a5636ded6de
shg-jmathews <jmathews@sledgehammergames.com>
2025-04-07 16:27:53 -0700

    added sort function and print statement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import get_num_words
 from stats import get_num_chars
+from stats import sort_characters
 
 # new function called get_book_text. It takes a filepath as input and returns the contents of the file as a string.
 def get_book_text(path_to_file):
@@ -7,14 +8,24 @@ def get_book_text(path_to_file):
         file_contents = f.read()
         return file_contents
 
+# Message to Print
+def formatted(path_to_file):
+    wrd_count = get_num_words(path_to_file)
+    sorted_characters = sort_characters(path_to_file)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {wrd_count} total words")
+    print("--------- Character Count -------")
+    for key in sorted_characters:
+        print(f"{key}: {sorted_characters[key]}")
+    print("============= END ===============")
+    return
+
 # a main function that uses get_book_text with the relative path to your frankenstein.txt file to print the entire contents of the book to the console.
 def main():
     filepath = "/home/jmathews/workspace/github.com/shg-jmathews/bookbot/books/frankenstein.txt"
-    text = get_book_text(filepath)
-    wrd_count = get_num_words(filepath)
-    print (wrd_count)
-    characters = get_num_chars(filepath)
-    print (characters)
+    formatted(filepath)
 
 
 # call main
--- stats.py
+++ stats.py
@@ -1,18 +1,16 @@
-#  new function that accepts the text from the book as a string, and returns the number of words in the string.
+#  accepts the text from the book as a string, and returns the number of words in the string.
 def get_num_words(path_to_file):
     try:
         with open(path_to_file, 'r') as file:
             content = file.read()
             words = content.split()
             word_count = len(words)
+            return word_count
     except FileNotFoundError:
         print(f"File '{path_to_file}' not found.")
         return 0
-    
-    total_words = f"{word_count} words found in the document"
-    return total_words
 
-# new function to your stats.py file that takes the text from the book as a string, and returns the number of times each character, (including symbols and spaces), appears in the string.
+# takes the text from the book as a string, and returns the number of times each character, (including symbols and spaces), appears in the string.
 def get_num_chars(path_to_file):
     try:
         with open(path_to_file, 'r') as file:
@@ -25,3 +23,21 @@ def get_num_chars(path_to_file):
     except FileNotFoundError:
         print(f"File '{path_to_file}' not found.")
         return 0
+    
+# takes the dictionary of characters and their counts and returns a sorted list of dictionaries.
+def sort_characters(path_to_file):
+    try:
+        with open(path_to_file, 'r') as file:
+            content = file.read()
+            lower = content.lower()
+            counts = {}
+            for char in lower:
+                if char.isalpha() == True:
+                    counts[char] = counts.get(char, 0) + 1
+            sorted_counts = dict(sorted(counts.items(), key=lambda item: item[1], reverse=True))
+            return sorted_counts
+    except FileNotFoundError:
+        print(f"File '{path_to_file}' not found.")
+        return 0
+
+
