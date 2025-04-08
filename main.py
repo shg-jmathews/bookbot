fb08784afda1773306c57dda72a8136cadb06cf7
shg-jmathews <jmathews@sledgehammergames.com>
2025-04-07 17:27:57 -0700

    added sys functions

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from stats import get_num_words
 from stats import get_num_chars
 from stats import sort_characters
+import sys
 
 # new function called get_book_text. It takes a filepath as input and returns the contents of the file as a string.
 def get_book_text(path_to_file):
@@ -13,7 +14,7 @@ def formatted(path_to_file):
     wrd_count = get_num_words(path_to_file)
     sorted_characters = sort_characters(path_to_file)
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {path_to_file}...")
     print("----------- Word Count ----------")
     print(f"Found {wrd_count} total words")
     print("--------- Character Count -------")
@@ -24,8 +25,13 @@ def formatted(path_to_file):
 
 # a main function that uses get_book_text with the relative path to your frankenstein.txt file to print the entire contents of the book to the console.
 def main():
-    filepath = "/home/jmathews/workspace/github.com/shg-jmathews/bookbot/books/frankenstein.txt"
-    formatted(filepath)
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        arguments = sys.argv[1:]
+        filepath = ' '.join(arguments)
+        formatted(filepath)
 
 
 # call main
